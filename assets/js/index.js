eff3ccf6ccac621f281a05839c4e5fee690c975a
Thành Phan <th.phan1408@gmail.com>
2023-08-15 10:30:57 +0700

    update dark-theme

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,14 @@
 // PreventDefault click button, tag <a>
-function disableClick(e){
-    e.preventDefault();
-    // console.log("run submit")
-} 
+function disableClick(e) {
+  e.preventDefault();
+  // console.log("run submit")
+}
+
+document.getElementById("icon__theme").onclick = function () {
+  //khi button được click thì tìm đến thẻ body có id là myBody
+  // dùng classlist để gọi thuộc tính class của thẻ body
+  //Sử dụng toggle() để thêm hoặc xóa 1 class của thẻ html. Nếu thẻ body không có class dark thì toggle sẽ thêm class dark vào body. Ngược lại, nếu thẻ body đang có class dark thì toogle sẽ xóa class dark đi.
+  document.getElementById("myBody").classList.toggle("dark");
+  
+  
+};
